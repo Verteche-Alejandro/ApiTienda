7608455e5c1e2d2f06a8f093ee760babddba14fd
abilec <abrilaladro@gmail.com>
2024-10-31 09:29:30 -0300

    Funciones de http para productos, pero no andan :c

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -17,13 +17,31 @@ using Microsoft.AspNetCore.Authorization;
 public class ProductosController : ControllerBase
 {
     private IConfiguration _config;
-    private Repository _repo;
+    private Repository basedatos;
 
     public ProductosController(IConfiguration config)
     {
         this._config = config;
-        this._repo = new Repository();
+        this.basedatos = new Repository();
     }
+    
+    [HttpGet("lista")]
+    [Authorize]
+    public async Task<BaseResponse> Lista(Productos productos)
+    {
+        try
+        {
+            string consulta = productos.SelectListProd();
+            var rsp = await this.basedatos.GetListBy<dynamic>(consulta);
+            return new DataResponse<dynamic>(false, (int)HttpStatusCode.Created,(""), rsp);
+        }
+        catch (Exception ex)
+        {
+            return new BaseResponse(false, (int)HttpStatusCode.Created, ex.Message);
+        }
+    }
+
+
     [HttpPost("alta")]
     [Authorize]
     public async Task<BaseResponse> Alta([FromBody] Productos productos)
@@ -35,17 +53,75 @@ public class ProductosController : ControllerBase
             try
             {
                 string consulta = productos.CreateProd();
-                var rsp = await this._repo.InsertByQuery(consulta);
+                var rsp = await this.basedatos.InsertByQuery(consulta);
                 return new BaseResponse(false, (int)HttpStatusCode.Created, "Producto nuevo agregado con Exito");
             }
             catch (Exception ex)
             {
-                return new BaseResponse(false, (int)HttpStatusCode.Created, ex.Message);
+                return new BaseResponse(false, (int)HttpStatusCode.BadRequest, ex.Message);
             }
         }
         else
         {
             return new BaseResponse(false,(int)HttpStatusCode.BadRequest,("No es un usuario Adminitrador"));
+        }
+    }
+
+    
+
+    [HttpDelete("baja/id={id}")]
+    [Authorize]
+    public async Task<BaseResponse> Baja([FromQuery] Productos productos, int id)
+    {
+        var user = HttpContext.User;
+        if (user.IsInRole("admin"))
+        {
+            try
+            {
+                string consulta = productos.DeleteProd(id);
+                var rsp = await this.basedatos.DeleteAsync(consulta);
+                return new BaseResponse(false, (int)HttpStatusCode.Created, ("Producto eliminado con exito"));
+            }catch (Exception ex)
+            {
+
+                return new BaseResponse(false, (int)HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+        else
+        {
+            return new BaseResponse(false, (int)HttpStatusCode.BadRequest, ("No es un usuario Adminitrador"));
+        }
+    }
+
+    [HttpPut("modificar")]
+    [Authorize]
+    public async Task<BaseResponse> Modi([FromBody] Productos productos)
+    {
+        var user = HttpContext.User;
+        if (user.IsInRole("admin"))
+        {
+
+            if(productos.id_prod != 0)
+            {
+                try
+                {
+                    string query = productos.UpdateProd(productos.nombre,productos.precio,productos.id_prod);
+                    var rsp = await this.basedatos.UpdateByQuery(query);
+                    return new BaseResponse(false, (int)HttpStatusCode.Created, ("Producto modificado correctamente"));
+                }
+                catch (Exception ex)
+                {
+                    return new BaseResponse(false, (int)HttpStatusCode.BadRequest, ex.Message);
+                }
+            }
+            else
+            {
+                return new BaseResponse(false, (int)HttpStatusCode.BadRequest, ("Se necesita un ID del producto"));
+            }
+        }
+        else
+        {
+            return new BaseResponse(false, (int)HttpStatusCode.BadRequest, ("No es un usuario Administrador"));
         }
     }
 
